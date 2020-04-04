17ddd1d5729ccd25e8b25d8caa1c3182285dde2b
JPrimo <juliozz59@gmail.com.com>
2020-04-04 11:57:26 -0700

    Added files for TCP client/server connection

---
+++ penetration-test/TCP/client.py
@@ -0,0 +1,10 @@
+#With this file you can create a simple server/client connection
+# This example runs a server on your local machine then run the client to establish
+# the communication among client server
+import socket
+
+sock_ = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock_.connect(("127.0.0.1",9337))
+msg = sock_.recv(1024) #How many bytes you want to receive
+sock_.close()
+print(msg.decode("ascii"))
---
+++ penetration-test/TCP/server.py
@@ -0,0 +1,20 @@
+#Basics of client/server TCP communication
+import socket
+
+host = "127.0.0.1"
+port = 9337
+
+sock_ = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+tupl=(host,port)
+sock_.bind(tupl)
+sock_.listen(1)
+
+print("\nServer started...\n")
+
+conn,addr = sock_.accept()
+
+print("Connection established with: ",str(addr))
+
+message = "\nThank you for connecting "+str(addr)
+conn.send(message.encode("ascii"))
+conn.close()
